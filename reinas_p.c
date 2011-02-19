98af07b4085c9e0e300e438e72acab5b4c218bf7
juliana <juliana@juliana-Aspire-3650.(none)>
2011-02-18 19:50:27 -0430

    PROCESOS CASI Y FUNCIONA

--- reinas_p.c
+++ reinas_p.c
@@ -4,7 +4,7 @@
  * Se asigna una tarea a cada proceso hijo para encontrar 
  * una solucion para dado tablero con una reina colocada.
  */   
-
+#include "8reinas.c"
 #include "estructuras.h"
 #include <string.h>
 FILE *arch;
@@ -44,53 +44,47 @@ main(int argc, char *argv[]){
   int hijo;
   int padre= getpid();
 
-  for (j=1;j-1<n/8;++j){
-    i=0;
-    for(i=1;i-1<n;++i){ // quizas con lo que ellos te dan de la posicion de la fila se pueda eliminar un for pero ahora no lo veo 
-      hijo=fork();
-      if (hijo<0){
-	perror("en el fork");
-	//exit(1);
+  for(i=0;i<n;++i){
+    hijo=fork();
+    if (hijo<0){
+      perror("fork");
+      exit(1);
+    }
+
+    else if( hijo==0){
+     
+     char cadena[20];
+     sprintf(cadena,"TI_%d_%d",i%8,i/8);
+      arch=fopen(cadena,"a+");
+      int nreinas=8;
+      int *reinas;
+      int sol;
+      reinas = (int*) malloc ( nreinas*sizeof(int) );
+      sol=sol_reinas(i%8,i/8,reinas,8);
+      for(j=0;j<8;j++){
+	fprintf(arch ,"%d\n",reinas[j]);
       }
+      fclose(arch); 
+      exit(0);
 
-      else if(hijo==0){
-	
-
-	trabajo(i,j);
-	//printf("Hijo: i:%d my pid:%ld mi padre es:%ld\n", i, (long)getpid(), (long)getppid());
-	exit(0);// falta considerar el error de exit
-      }
-    
-      else{
-
-	int estado;
-
-	//printf("Hijo: i:%d my pid:%ld mi padre es:%ld\n", i, (long)getpid(), (long)getppid());
-	wait(&estado);
-	//printf("Termino:i:%d pid:%ld\n", i, (long)getpid());
-	if (WEXITSTATUS(estado)!=0){
+    }
+    else {
+      int estado;
+      wait(&estado);
+      	if (WEXITSTATUS(estado)!=0){
 	  perror("termino mal un hijo");
 	  exit(1);
        	}
-      }
     }
   }
-  for(i=0;i<n;i++){
-    char cadena[10];
-    sprintf(cadena,"TI_%d",i+1);
-    strcat(cadena,"_");
-    //int solu[10];
-    
+  for(i=0;i<n;++i){
+    char cadena[20];
+    sprintf(cadena,"TI_%d_%d",i%8,i/8);
     arch=fopen(cadena,"r");
-    j=0;
-    int end=0;
-    while(j<=8){//poner comdicion de parar al terminar el archivo
-      end=fscanf(arch,"%d",&Soluciones[j][i]);
-      //printf("%d\n",Soluciones[j][i]);
-      
-      j++;
+    for(j=0;j<8;j++){
+      fscanf(arch,"%d",&Soluciones[i][j]);
     }
   }
-}
+}     
 
 
